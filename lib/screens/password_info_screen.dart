5dd1067f077d3f8d8815f9e98d77355924cec7cb
Erick Lopez <93694994+er-lo@users.noreply.github.com>
2024-02-19 20:43:59 -0600

    added delete function and also fixed overflow issue on text boxes

--- lib/screens/password_info_screen.dart
+++ lib/screens/password_info_screen.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:password_manager/models/password_database.dart';
 import 'package:password_manager/models/password_model.dart';
 import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 class PasswordInformationPage extends StatefulWidget {
   const PasswordInformationPage({super.key, required this.password});
@@ -20,35 +24,47 @@ class _PasswordInformationPageState extends State<PasswordInformationPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Pass Vault"),
         actions: [
-          TextButton(
-            onPressed: () {},
-            child: const Text(
-              "EDIT",
-              style: TextStyle(letterSpacing: 1.1),
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.edit),
             ),
           ),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(8),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            listTile("WEBSITE / APP", widget.password.website, false),
-            listTile("EMAIL", widget.password.email, false),
-            listTile("USERNAME", widget.password.userName, false),
-            listTile("PASSWORD", widget.password.password, true),
-            listTile("NOTES", widget.password.note, false),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              listTile("WEBSITE / APP", widget.password.website, false, true),
+              listTile("EMAIL", widget.password.email, false, true),
+              listTile("USERNAME", widget.password.userName, false, true),
+              listTile("PASSWORD", widget.password.password, true, true),
+              listTile("NOTES", widget.password.note, false, false),
+              OutlinedButton(
+                onPressed: () {
+                  context
+                      .read<PasswordDatabase>()
+                      .deletePassword(widget.password.id);
+                  Navigator.pop(context);
+                },
+                child: const Text("DELETE"),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget listTile(String title, String subTitle, bool passwordTile) {
+  Widget listTile(
+      String title, String subTitle, bool passwordTile, bool copyTile) {
+    var deviceSize = MediaQuery.of(context).size;
+    var width = deviceSize.width;
     return Padding(
       padding: const EdgeInsets.all(16),
       child: Row(
@@ -65,38 +81,46 @@ class _PasswordInformationPageState extends State<PasswordInformationPage> {
                 ),
               ),
               const SizedBox(height: 8),
-              passwordTile
-                  ? passwordIcon
-                      ? Text(subTitle)
-                      : const Text("*********")
-                  : Text(subTitle)
+              LimitedBox(
+                maxWidth: width / 1.3,
+                child: passwordTile
+                    ? passwordIcon
+                        ? Text(subTitle)
+                        : const Text("*********")
+                    : Text(subTitle),
+              )
             ],
           ),
-          Row(
-            children: [
-              Visibility(
-                visible: passwordTile,
-                child: IconButton(
+          Visibility(
+            visible: copyTile,
+            child: Row(
+              children: [
+                Visibility(
+                  visible: passwordTile,
+                  child: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        passwordIcon = !passwordIcon;
+                      });
+                    },
+                    icon: Icon(
+                        passwordIcon ? Icons.visibility : Icons.visibility_off),
+                  ),
+                ),
+                IconButton(
                   onPressed: () {
-                    setState(() {
-                      passwordIcon = !passwordIcon;
+                    Clipboard.setData(ClipboardData(text: subTitle)).then((_) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("Succesfully copied to clipboard!"),
+                        ),
+                      );
                     });
                   },
-                  icon: passwordIcon
-                      ? const Icon(Icons.visibility_off)
-                      : const Icon(Icons.visibility),
+                  icon: const Icon(Icons.copy),
                 ),
-              ),
-              IconButton(
-                onPressed: () {
-                  Clipboard.setData(ClipboardData(text: subTitle)).then((_) {
-                    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text("Succesfully copied to clipboard!")));
-                  });
-                },
-                icon: const Icon(Icons.copy),
-              ),
-            ],
+              ],
+            ),
           ),
         ],
       ),
